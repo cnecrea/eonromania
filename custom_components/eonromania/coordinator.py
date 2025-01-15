712d309ea83feddb5c37cd4a8ec5defdd1759aa8
Ciprian Nicolae <48139709+cnecrea@users.noreply.github.com>
2025-01-15 08:07:16 +0200

    Update coordinator.py
    Am adăugat logica pentru colectarea și procesarea datelor lunare și zilnice utilizate de senzorul ArhivaComparareConsumAnualGraficSensor.

--- custom_components/eonromania/coordinator.py
+++ custom_components/eonromania/coordinator.py
@@ -34,6 +34,7 @@ class EonRomaniaCoordinator(DataUpdateCoordinator):
         dateuser_data = await self.api_client.async_fetch_dateuser_data(self.cod_incasare)
         citireindex_data = await self.api_client.async_fetch_citireindex_data(self.cod_incasare)
         conventieconsum_data = await self.api_client.async_fetch_conventieconsum_data(self.cod_incasare)
+        comparareanualagrafic_data = await self.api_client.async_fetch_comparareanualagrafic_data(self.cod_incasare)
         arhiva_data = await self.api_client.async_fetch_arhiva_data(self.cod_incasare)
         facturasold_data = await self.api_client.async_fetch_facturasold_data(self.cod_incasare)
 
@@ -44,6 +45,7 @@ class EonRomaniaCoordinator(DataUpdateCoordinator):
             "dateuser": dateuser_data,
             "citireindex": citireindex_data,
             "conventieconsum": conventieconsum_data,
+            "comparareanualagrafic": comparareanualagrafic_data,
             "arhiva": arhiva_data,
             "facturasold": facturasold_data,
             "payments": payments_data,
